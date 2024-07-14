9dcb169080c2748497be80ecf25b66094fe87879
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-07-13 23:40:22 -0300

    Update UserController.java

--- src/main/java/com/aleiku/springboot/web/spring/controllers/UserController.java
+++ src/main/java/com/aleiku/springboot/web/spring/controllers/UserController.java
@@ -1,5 +1,13 @@
 package com.aleiku.springboot.web.spring.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class UserController {
+
+    @PostMapping
+    public void helloWorld(){
+        System.out.println("Hello World!");
+
+    }
 
 }

ea3b6a8df5c22985cdaac59dd6d1f8adc1e9651d
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-07-14 22:51:21 -0300

    Cambiando nombre en controller

--- src/main/java/com/aleiku/springboot/web/spring/controllers/UserController.java
+++ src/main/java/com/aleiku/springboot/web/spring/controllers/UserController.java
@@ -2,10 +2,6 @@ package com.aleiku.springboot.web.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 
 @Controller
@@ -14,7 +10,7 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model){
         model.addAttribute("title", "Hola mundo");
-        model.addAttribute("name", "Alejadra");
+        model.addAttribute("name", "Alejandra");
         model.addAttribute("lastname", "Araya Díaz");
         return "details";
     }

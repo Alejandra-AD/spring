5348b49397c0f9dd60dafd8dfe74ec5a36009ee6
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-07-19 20:43:11 -0300

    Create UserRestController.java

---
+++ src/main/java/com/aleiku/springboot/web/spring/controllers/UserRestController.java
@@ -0,0 +1,26 @@
+package com.aleiku.springboot.web.spring.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class UserRestController {
+
+    @GetMapping("/details2")
+    public String details2(){
+
+        Map< String, Object> jsonBody = new HashMap<>();
+        jsonBody.put("nombre", "Rocco");
+
+        return "details2";
+
+    }
+
+}
